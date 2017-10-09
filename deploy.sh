a28d7352cc52f3bb705f7d36fc3beb9c6ccb7a11
Your Name <sergey_umnov@mail.ru>
2017-10-10 02:00:17 +0300

    change server script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-docker build -t kostyaurysov/sample-node .
-docker push kostyaurysov/sample-node
+docker push jeaced/sample-node
 
-ssh deploy@35.187.30.81 << EOF
-docker pull kostyaurysov/sample-node:latest
+ssh sergey_umnov@35.195.239.193  << EOF
+docker pull jeaced/sample-node:latest
 docker stop web || true
 docker rm web || true
-docker rmi kostyaurysov/sample-node:current || true
-docker tag kostyaurysov/sample-node:latest kostyaurysov/sample-node:current
-docker run -d --net app --restart always --name web -p 80:80 kostyaurysov/sample-node:current
+docker rmi jeaced/sample-node:current || true
+docker tag jeaced/sample-node:latest kostyaurysov/sample-node:current
+docker run -d --net app --restart always --name web -p 80:80 jeaced/sample-node:current
 EOF
