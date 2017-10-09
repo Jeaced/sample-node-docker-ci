b8f4a703fb876200125cd8a34c7bf6f207912d0c
Your Name <sergey_umnov@mail.ru>
2017-10-10 02:51:10 +0300

    should work this time

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,10 @@
 docker push jeaced/sample-node
 
 ssh sergey_umnov@35.195.239.193 << EOF
-docker pull jeaced/sample-node:latest
-docker stop web || true
-docker rm web || true
-docker rmi jeaced/sample-node:current || true
-docker tag jeaced/sample-node:latest jeaced/sample-node:current
-docker run -d --restart always --name web -p 80:80 jeaced/sample-node:current
+sudo docker pull jeaced/sample-node:latest
+sudo docker stop web || true
+sudo docker rm web || true
+sudo docker rmi jeaced/sample-node:current || true
+sudo docker tag jeaced/sample-node:latest jeaced/sample-node:current
+sudo docker run -d --restart always --name web -p 80:80 jeaced/sample-node:current
 EOF
